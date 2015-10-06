c0ed0ff5d12d9397fc70f1b9196b716a62437930
Adrien Manikon <adrien.manikon@enyx.ch>
2015-10-06 17:42:38 +0200

    get new line by ajax

--- src/SW/DocManagerBundle/Controller/AddController.php
+++ src/SW/DocManagerBundle/Controller/AddController.php
@@ -74,7 +74,7 @@ class AddController extends Controller
             'form' => $form->createView(),
         ));
     }
-    
+        
     public function recapAction()
     {
         return $this->render('SWDocManagerBundle:Add:recap.html.twig');
@@ -87,5 +87,21 @@ class AddController extends Controller
      */
     public function isMethodPost() {
         return $this->request->isMethod("POST");
+    }
+    
+    public function getNewInputDocumentAction() {
+        $document = new Document();
+        $document->setCode("DEEC2A");
+        $document->setDate(new DateTime("2012-07-08"));
+        
+        /* TEST */
+        $document->setName("test2.pdf");
+        $document->setInitials("AM");
+        
+        $form = $this->get('form.factory')->create(new DocumentType, $document);
+        
+        return $this->render('SWDocManagerBundle:Add:inputline.html.twig', array(
+            'form' => $form->createView(),
+        )); 
     }
 }
--- src/SW/DocManagerBundle/Resources/public/js/upload.js
+++ src/SW/DocManagerBundle/Resources/public/js/upload.js
@@ -1,12 +1,32 @@
 var nbinput = 1;
 
-function addFileInput() {
-    
+function addFileInput(element) {
+        
     if(nbinput < 5) {
         nbinput++;
+        
+        var url = element.getAttribute("data-url");
+        
+        $.ajax({
+            type: "POST",
+            url: url,
+            async: true,
+            dataType: "html",
+            success: function(response) 
+            {
+                //console.log(response);
+                var newline = jQuery.parseHTML(response);
+                //newline.hide();
+                $("#inputBlock").append(response);
+            },
+            error: function(XMLHttpRequest, textStatus, errorThrown)
+            {
+                // stuff there
+            },
+        });/*
         var $newline = $("#fileInput1").clone().prop('id', "fileInput" + nbinput);
         $newline.hide();
-        $newline.appendTo("#inputBlock").show("slow");
+        $newline.appendTo("#inputBlock").show("slow");*/
     }else {
         showDialog("#limitFileDialog");
     }        
