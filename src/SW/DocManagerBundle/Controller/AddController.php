9c45cd44ec440c280709d680a00424077fda4f5c
Adrien Manikon <adrien.manikon@enyx.ch>
2015-10-07 13:24:19 +0200

    change route

--- src/SW/DocManagerBundle/Controller/AddController.php
+++ src/SW/DocManagerBundle/Controller/AddController.php
@@ -26,6 +26,13 @@ class AddController extends Controller
         
     private $uploadSession;
     
+    private $category;
+    
+    private $subcategory1;
+    
+    private $subcategory2;
+    
+    private $subcategory3;
     /**
      *
      * @var Request 
@@ -36,23 +43,20 @@ class AddController extends Controller
     {        
         $this->request = $request;
         
-        if ($this->isMethodPost()) {   
-            $category = $request->request->get("category");
-            $subcategory1 = $request->request->get("select1");
-            $subcategory2 = $request->request->get("select2");
-            $subcategory3 = $request->request->get("select3");
-            return $this->redirect($this->generateUrl('sw_doc_manager_upload_view', array(
-                'category' => $category,
-                'subcategory1' => $subcategory1,
-                'subcategory2' => $subcategory2,
-                'subcategory3' => $subcategory3,
-            )));
+        if ($this->isMethodPost()) {
+            
+            $this->category = $request->request->get("category");
+            $this->subcategory1 = $request->request->get("select1");
+            $this->subcategory2 = $request->request->get("select2");
+            $this->subcategory3 = $request->request->get("select3");
+            return $this->uploadViewAction($request);
+            
         }
         
         return $this->render('SWDocManagerBundle:Add:categorylist.html.twig');
     }
     
-    public function uploadViewAction($category, $subcategory1, $subcategory2, $subcategory3, Request $request)
+    public function uploadViewAction(Request $request)
     {       
         
         $uploadSession = new UploadSession();
@@ -74,7 +78,7 @@ class AddController extends Controller
         }
         
         return $this->render('SWDocManagerBundle:Add:upload.html.twig', array(
-            'category' => $category,
+            'category' => $this->category,
             'subcategory1' => "madera",
             'subcategory2' => "Wämeschutz",
             'subcategory3' => "technische",
@@ -94,21 +98,5 @@ class AddController extends Controller
      */
     public function isMethodPost() {
         return $this->request->isMethod("POST");
-    }
-    
-    public function getNewInputDocumentAction() {
-        $document = new Document();
-        $document->setCode("DEEC2A");
-        $document->setDate(new DateTime("2012-07-08"));
-        
-        /* TEST */
-        $document->setName("test2.pdf");
-        $document->setInitials("AM");
-        
-        $form = $this->get('form.factory')->create(new DocumentType, $document);
-        
-        return $this->render('SWDocManagerBundle:Add:inputline.html.twig', array(
-            'form' => $form->createView(),
-        )); 
     }
 }
