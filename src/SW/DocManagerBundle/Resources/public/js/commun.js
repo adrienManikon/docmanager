5bc9b64392f1000e378684b57888a09c33510fcc
Adrien Manikon <adrien.manikon@enyx.ch>
2015-10-14 13:56:16 +0200

    responsive design

--- src/SW/DocManagerBundle/Resources/public/js/commun.js
+++ src/SW/DocManagerBundle/Resources/public/js/commun.js
@@ -13,11 +13,11 @@ function fillTable(documents) {
     var $url = $("#documents-table").attr("data-url").replace("/-1", "");
     var $table = $('<table class="table"></table>');
     thead ='<thead>';
-    thead += '<tr>';
-    thead += '<th class="sortable-column sort-desc">Name</th>';
-    thead += '<th class="sortable-column sort-desc">Datum</th>';
-    thead += '<th class="sortable-column">Bezeichnung</th>';
-    thead += '<th class="sortable-column">Von</th>';
+    thead += '<tr class="row cells7">';
+    thead += '<th class="cell sortable-column">Name</th>';
+    thead += '<th class="cell sortable-column">Datum</th>';
+    thead += '<th class="cell sortable-column">Bezeichnung</th>';
+    thead += '<th class="cell sortable-column">Von</th>';
     thead += '</tr>';
     thead += '</thead>';
     
@@ -25,15 +25,15 @@ function fillTable(documents) {
     
     documents.forEach(function(document){
                 
-        var $td = '<td><span class="mif-file-pdf"></span> ' + document.name + '</td>';
-        $td += '<td>' + document.date + '</td>';
-        $td += '<td>' + document.code + '</td>';
-        $td += '<td>' + document.creator + '</td>';
-        $td += '<td><span class="edit-document mif-pencil" onclick=\'editDocument(' + document.id + ', \"' + document.name + '\", \"' + document.date + '\", \"' + document.code + '\", \"' + document.creator + '\")\' data-id="' + document.id + '"></span></td>';
-        $td += '<td><a target="_blank" href="' + $url + '/' + document.id + '"><span class="mif-search"></span></a></td>';
-        $td += '<td><span class="delete-document mif-bin" onclick=\'deleteDocument(' + document.id + ', \"' + document.name + '\")\' data-id="' + document.id + '"></span></td>';
+        var $td = '<td class="cell"><span class="mif-file-pdf"></span> ' + document.name + '</td>';
+        $td += '<td class="cell">' + document.date + '</td>';
+        $td += '<td class="cell">' + document.code + '</td>';
+        $td += '<td class="cell">' + document.creator + '</td>';
+        $td += '<td class="cell"><span class="edit-document mif-pencil" onclick=\'editDocument(' + document.id + ', \"' + document.name + '\", \"' + document.date + '\", \"' + document.code + '\", \"' + document.creator + '\")\' data-id="' + document.id + '"></span></td>';
+        $td += '<td class="cell"><a target="_blank" href="' + $url + '/' + document.id + '"><span class="mif-search"></span></a></td>';
+        $td += '<td class="cell"><span class="delete-document mif-bin" onclick=\'deleteDocument(' + document.id + ', \"' + document.name + '\")\' data-id="' + document.id + '"></span></td>';
         
-        $newRow = $('<tr></tr>').append($td);
+        $newRow = $('<tr class="row cells7"></tr>').append($td);
         
         $table.append($newRow);
         
