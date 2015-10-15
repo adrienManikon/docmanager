6898e5c406bec33a3fa951edf81ef3003ee97fb4
Adrien Manikon <adrien.manikon@enyx.ch>
2015-10-15 09:03:09 +0200

    delete useless entities and bundles

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,9 +16,6 @@ class AppKernel extends Kernel
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
-            new AppBundle\AppBundle(),
-            new Doctrine\Bundle\PHPCRBundle\DoctrinePHPCRBundle(),
-            new Liip\ImagineBundle\LiipImagineBundle(),
             new SW\DocManagerBundle\SWDocManagerBundle(),
         );
 
@@ -27,7 +24,6 @@ class AppKernel extends Kernel
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
             $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
-            $bundles[] = new Symfony\Cmf\Bundle\MediaBundle\CmfMediaBundle();
             $bundles[] = new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle();
         }
 
